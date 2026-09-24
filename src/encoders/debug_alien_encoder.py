c649f997924ea8b020034695c5753dd235689385
es19kuhy <esther.song@stud.tu-darmstadt.de>
2026-09-24 16:04:13 +0200

    attempt at Alien

--- src/encoders/debug_alien_encoder.py
+++ src/encoders/debug_alien_encoder.py
@@ -3,13 +3,11 @@ import jax.numpy as jnp
 from jax import lax
 from numpy.random import seed
 
-from jaxatari.src import jaxatari
-from jaxatari.src import jaxatari
 from encoders.game_encoder import GameEncoder
-from navigation import build_legal_moves, plan
-from jaxatari.games.jax_alien import get_level_maze
 import jaxatari.games.jax_alien as game
 import alien_encoder as alien_encoder
+from alien_encoder import make_alien_encoder
+
 
 
 TILE = 4
@@ -19,11 +17,13 @@ PLAYER_FOOTPRINT = (8, 13)
 
 env = jaxatari.make("alien")
 obs, state = env.reset(jax.random.PRNGKey(seed))
+encoder = make_alien_encoder(env)
+
 eggs = game.state.eggs
 
-gx, gy = alien_encoder._egg_indices(eggs, walkable.shape)
+gx, gy = alien_encoder._egg_indices(eggs, encoder.walkable.shape)
 
-print("walkable shape:", walkable.shape)
+print("walkable shape:", encoder.walkable.shape)
 
 print("first 10 egg positions:")
 print(eggs[0, :10, :2])
@@ -33,4 +33,4 @@ print(gx[0, :10])
 print(gy[0, :10])
 
 print("walkable at egg positions:")
-print(walkable[gx[0, :10], gy[0, :10]])
+print(encoder.walkable[gx[0, :10], gy[0, :10]])
